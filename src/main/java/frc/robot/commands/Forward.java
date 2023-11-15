a483c08587516202bc523ec077adde81ef4ca8be
KinetekEnergy <transyt8@gmail.com>
2023-11-15 15:32:55 -0800

    formatting

--- src/main/java/frc/robot/commands/Forward.java
+++ src/main/java/frc/robot/commands/Forward.java
@@ -9,32 +9,32 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class Forward extends CommandBase {
-  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" }) //ignore this
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" }) // ignore this
 
   final Drivetrain m_Drivetrain;
 
   // forward command; define and inherite drivetrain subsystem
   public Forward(Drivetrain drivetrain) {
     m_Drivetrain = drivetrain;
-    addRequirements(drivetrain); //dependencies (IMPORTANT)
+    addRequirements(drivetrain); // dependencies (IMPORTANT)
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_Drivetrain.tankDrive(0, 0); //first start at position 0
+    m_Drivetrain.tankDrive(0, 0); // first start at position 0
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_Drivetrain.tankDrive(0.5, 0.5); //ok, now lets move the drive forward by this much
+    m_Drivetrain.tankDrive(0.5, 0.5); // ok, now lets move the drive forward by this much
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_Drivetrain.tankDrive(0, 0); //stop motors on the interruption
+    m_Drivetrain.tankDrive(0, 0); // stop motors on the interruption
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -12,13 +12,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase {
-  //define the motor objects for all 4 wheels
+  // define the motor objects for all 4 wheels
   WPI_TalonFX leftFront = new WPI_TalonFX(Constants.Drivetrain.leftFront);
   WPI_TalonFX rightFront = new WPI_TalonFX(Constants.Drivetrain.rightFront);
   WPI_TalonFX leftBack = new WPI_TalonFX(Constants.Drivetrain.leftBack);
   WPI_TalonFX rightBack = new WPI_TalonFX(Constants.Drivetrain.rightBack);
 
-  //controller groups for each side
+  // controller groups for each side
   MotorControllerGroup m_left = new MotorControllerGroup(leftFront, leftBack);
   MotorControllerGroup m_right = new MotorControllerGroup(rightFront, rightBack);
 
@@ -31,6 +31,7 @@ public class Drivetrain extends SubsystemBase {
     m_left.set(left);
     m_right.set(right);
   }
+
   /**
    * Example command factory method.
    *
